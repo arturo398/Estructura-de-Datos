a3a288ca9911678a556324c9eeca6d51dc7e0910
Arturo Balbi <balbiarturo@gmail.com>
2024-10-16 12:45:57 -0300

    Tp3

--- TP3/Ejercicio3_P3.py
+++ TP3/Ejercicio3_P3.py
@@ -4,19 +4,21 @@ cola= []
     return nombre
 def atender(cola):
    ''' 
-do {
-print("-----  Menu  ------")
-print("1- Cargar cliente")
-print("2- Atender cliente")
-print("3- Consultar clientes restantes")
-op= int(input("Ingrese una opcion: "))
-match op:
-    case '1':
-        cola.append(cargar())
-    case '2':
-        cola[0].pop()
-    case '3':
-         print(f"Quedan {len(cola)} en la cola y son: ")
-         print(cola)
-
-    }while(op!= 'S' or 's')
+op=0
+while(op!= 4):
+    print("-----  Menu  ------")
+    print("1- Cargar cliente")
+    print("2- Atender cliente")
+    print("3- Consultar clientes restantes")
+    op= int(input("Ingrese una opcion: "))
+    if op== 1 :
+        cliente= input("Ingrese nombre del cliente: ")
+        cola.append(cliente)
+    elif op== 2 :
+        
+        del cola[0]
+    elif op== 3 :
+        print(f"Quedan {len(cola)} en la cola y son: ")
+        print(cola)
+    elif op== 4 :
+        print("Cerrando el sistema de atencion al cliente")
